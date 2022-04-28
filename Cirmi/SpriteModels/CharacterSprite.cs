cf7554b365b55b9c7074e65077bed20111bf04d9
ZUSI3U <wertigo@stud.uni-obuda.hu>
2022-04-28 22:06:43 +0200

    Added sprite models for easier display

---
+++ Cirmi/SpriteModels/CharacterSprite.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Cirmi.SpriteModels
+{
+    public class CharacterSprite : Sprite
+    {
+		private readonly List<ImageSource> animations;
+		int idleFrame;
+
+        public DispatcherTimer animTimer;
+		bool animTimerIsRunning = false;
+		int frameDuration = 100;
+		int animIndex = 0;
+		int direction = 0;
+
+		public CharacterSprite(List<ImageSource> animations, int idleFrame = 1)
+        {
+			this.animations = animations;
+			this.idleFrame = idleFrame;
+			Brush = new ImageBrush(animations[idleFrame]);
+			animTimer = new DispatcherTimer();
+			animTimer.Tick += (sender, e) => NextFrame();
+		}
+
+		public int Direction { get { return direction; } set { direction = value; } }
+
+		private void SetCurrentFrame(ImageSource source) { Brush.ImageSource = source; }
+
+		/// <summary>
+		/// Start sprite animation.
+		/// </summary>
+		public void StartAnimation()
+		{
+			if(!animTimerIsRunning)
+            {
+				animTimerIsRunning = true;
+				animTimer.Interval = TimeSpan.FromMilliseconds(frameDuration);
+				animTimer.Start();
+			}
+		}
+
+		/// <summary>
+		/// Pause running sprite animation
+		/// </summary>
+		public void PauseAnimation()
+		{
+			if (animTimerIsRunning)
+			{
+				SetCurrentFrame(animations[1 + (direction * 3)]);
+				animTimer.Stop();
+				animTimerIsRunning = false;
+			}
+		}
+
+		/// <summary>
+		/// Increase animation index by one or reset to zero if at end of list
+		/// </summary>
+		private void NextFrame()
+		{
+			animIndex++;
+			animIndex %= 3;
+			SetCurrentFrame(animations[animIndex+(direction*3)]);
+		}
+	}
+}
---
+++ Cirmi/SpriteModels/SimpleSprite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Cirmi.SpriteModels
+{
+    public class SimpleSprite : Sprite
+	{
+		public SimpleSprite(ImageSource image)
+        {
+			if(image != null)
+				Brush = new ImageBrush(image);
+
+			//spriteFrame = CutSheet(spriteSheet, row, col);
+		}
+
+		/*
+        public int Row { get; }
+        public int Col { get; }
+
+        private ImageBrush CutSheet(BitmapImage spriteSheet, int row, int col)
+		{
+			Int32Rect cropRect = new Int32Rect(0, 0, frameWidth, frameHeight);
+			BitmapImage sheet = spriteSheet;
+
+			if ((sheet.PixelHeight % frameHeight) == 0 && (sheet.PixelWidth % frameWidth) == 0)
+			{
+				int currentY = row * frameHeight;
+				int currentX = col * frameWidth;
+				cropRect.X = currentX;
+				cropRect.Y = currentY;
+				return new ImageBrush(new CroppedBitmap(sheet, cropRect));
+			}
+
+			return new ImageBrush();
+		}*/
+	}
+}
